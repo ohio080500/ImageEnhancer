d8e34204de2f1c0e34b8ff39b8e7e7d069101f6e
Ryuuji Takasu <mileslaurencejubinal@yahoo.com>
2026-06-02 20:50:45 +0800

    owno

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,99 +1,120 @@
 import colors from 'vuetify/es5/util/colors'
 
+console.log('GOOGLE_CLIENT_ID:', process.env.GOOGLE_CLIENT_ID)
+console.log('REDIRECT_URI:', process.env.REDIRECT_URI)
+
 export default {
   mode: 'spa',
-  /*
-  ** Headers of the page
-  */
+
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  /*
-  ** Customize the progress-bar color
-  */
+
   loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
+
+  css: [],
+
   plugins: [
     '~/plugins/image-enhancer.js'
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
+
   buildModules: [
-    '@nuxtjs/vuetify',
+    '@nuxtjs/vuetify'
   ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: ['@nuxtjs/axios',
+
+  modules: [
+    '@nuxtjs/axios',
     '@nuxtjs/auth-next'
   ],
+
   auth: {
-    redirect:{
+    redirect: {
       login: '/auth/login',
       logout: '/auth/login',
       callback: '/auth/callback',
-      home:'/'
+      home: '/'
     },
+
     autoFetchUser: false,
+
     strategies: {
       google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      scheme:'oauth2',
-      endpoints:{
-        authorization: 'https://accounts.google.com/o/oauth2/auth',
-        token: 'https://oauth2.googleapis.com/token',
-        userInfo: "https://www.googleapis.com/oauth2/v3/userinfo",
+        scheme: 'oauth2',
+
+        clientId:
+          '681717550297-s47pr1ifo65jabmn5p9b8ao1q1dajkfu.apps.googleusercontent.com',
+
+        endpoints: {
+          authorization: 'https://accounts.google.com/o/oauth2/auth',
+          token: 'https://oauth2.googleapis.com/token',
+          userInfo: 'https://www.googleapis.com/oauth2/v3/userinfo'
+        },
+
+        token: {
+          property: 'access_token',
+          type: 'Bearer',
+          maxAge: 1800
+        },
+
+        responseType: 'token id_token',
+
+        scope: [
+          'openid',
+          'profile',
+          'email'
+        ],
+
+        redirectUri:
+          'https://nuxt-enhancer.vercel.app/auth/callback',
+
+        codeChallengeMethod: ''
       },
-      token:{
-        property: "access_token",
-        type: "Bearer",
-        maxAge: 1800,
-      },
-      responseType: "token id_token",
-      scope: ["openid","profile","email"],
-      redirectUri: process.env.REDIRECT_URI,
-      codeChallengeMethod: "",
-    },
+
       github: {
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
+
         scheme: 'oauth2',
-    endpoints: {
-      authorization: 'https://github.com/login/oauth/authorize',
-      token: 'https://github.com/login/oauth/access_token',
-      userInfo: 'https://api.github.com/user'
-    },
-    responseType: "token id_token",
-    scope: ['user', 'email'],
-    redirectUri: process.env.REDIRECT_URI,
-    codeChallengeMethod: ''
+
+        endpoints: {
+          authorization: 'https://github.com/login/oauth/authorize',
+          token: 'https://github.com/login/oauth/access_token',
+          userInfo: 'https://api.github.com/user'
+        },
+
+        responseType: 'token id_token',
+
+        scope: [
+          'user',
+          'email'
+        ],
+
+        redirectUri: process.env.REDIRECT_URI,
+
+        codeChallengeMethod: ''
+      }
+    }
   },
-    },},
-  /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
+
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+
     theme: {
       dark: true,
+
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -107,14 +128,8 @@ export default {
       }
     }
   },
-  /*
-  ** Build configuration
-  */
+
   build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+    extend(config, ctx) {}
   }
 }
